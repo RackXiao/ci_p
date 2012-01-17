30d94e188bd87ea904bcce86f7cad762d3b9c750
rick@qyoung.com.tw <rick@qyoung.com.tw@18986f20-e846-db75-509b-25390d23ae8d>
2012-01-17 08:10:56 +0000

    臨時建一個專案作品列表
    by R

---
+++ rackx/project.php
@@ -0,0 +1,73 @@
+<pre>
+A.專案名稱：今週刊發報系統 <a target="_blank" href="http://ms01.qyoung.com.tw/">http://ms01.qyoung.com.tw/</a>  [ 100/4 ~ now 共計10個月 ]
+專案人數：3
+專案角色：SA / PG
+使用技術：PHP、MySQL、Codeigniter、AJAX、jQuery、SMTP、Postfix
+作業環境：Linux(CentOS)。
+
+專案概述：今週刊電子報發送。包含會員資料管理、發送電子報統計、電子報發送排程、
+
+工作內容：需求分析、功能分析、系統設計、Coding、Debug、專案上線。
+
+
+
+B.專案名稱：赫士盟人力資源網 <a target="_blank" href="http://hr.hasmore.qyoung.com.tw/">http://hr.hasmore.qyoung.com.tw/</a>  [ 99/12 ~ 100/02 共計3個月 ]
+專案人數：4
+專案角色：SA / PG
+使用技術：PHP、SQLite、Codeigniter、AJAX、jQuery
+作業環境：Linux(CentOS)。
+
+專案概述：企業內部的人力資源網站。包含了一般使用者面試報名，個人資料管理；主管對應徵人員的面試結果處理；人資人員對企業內部應徵流程的管理，及面試人員錄用的管理；內部員工心理測驗的模組等。
+
+工作內容：需求分析、功能分析、系統設計、Coding、Debug。
+
+
+
+C.專案名稱：新晨旅行社 <a target="_blank" href="http://yohotour.com.tw/">http://yohotour.com.tw/</a>  [ 99/11 ~ now 共計15個月 ]
+專案人數：3
+專案角色：SA / SD / PG
+使用技術：PHP、MySQL、Codeigniter、AJAX、jQuery
+作業環境：Linux(CentOS)。
+
+專案概述：旅行社網站，提供企業及一般會員旅遊的線上報名。系統包含了各種不同形式的活動管理及線上報名。
+
+工作內容：需求分析、功能分析、系統設計、Coding、Debug、專案上線。
+
+
+
+D.專案名稱：Anden Hud <a target="_blank" href="http://www.andenhud.com/">http://www.andenhud.com/</a>  [ 99/10 ~ now 共計16個月 ]
+專案人數：3
+專案角色：SA / PG
+使用技術：PHP、MySQL、Codeigniter、AJAX、jQuery、SSL、J2SE、shell script
+作業環境：Linux(CentOS)。
+
+專案概述：線上服飾購物網站。系統包含了產品展示，購物車，訂單處理流程，會員中心，電子報寄送，7-11的物流功能串接，線上金流(信用卡及線上ATM等功能)；並為該網站提示系統及資料備援機制。
+
+工作內容：需求分析、功能分析、系統設計、Coding、Debug、專案上線。
+
+
+
+E.專案名稱：味自慢 <a target="_blank" href="http://www.ajijiman.com.tw/">http://www.ajijiman.com.tw/</a>  [ 99/9 ~ now 共計17個月 ]
+專案人數：3
+專案角色：PG
+使用技術：PHP、MySQL、Codeigniter、AJAX、jQuery
+作業環境：Linux(CentOS)。
+
+專案概述：味自慢 線上官方形象網站，食品、餐飲介紹，會員折扣列印。
+
+工作內容：需求分析、功能分析、系統設計、Coding、Debug、專案上線。
+
+
+
+F.專案名稱：伊聖詩芳療生活館 <a target="_blank" href="http://www.escents.com.tw/">http://www.escents.com.tw/</a>  [ 99/9 ~ now 共計17個月 ]
+專案人數：3
+專案角色：PG
+使用技術：PHP、MySQL
+作業環境：Linux(CentOS)。
+
+專案概述：系統修改及維護，電子報寄送平台(http://ms03.qyoung.com.tw)。
+
+工作內容：需求分析、功能分析、系統設計、Coding、Debug、專案上線。
+</pre>
+
+<?php
