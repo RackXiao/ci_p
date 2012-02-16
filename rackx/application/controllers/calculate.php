fd7a539f20e7bdee39f5a94d0de9f6238b7fed85
kongtern@gmail.com <kongtern@gmail.com@18986f20-e846-db75-509b-25390d23ae8d>
2012-02-16 14:35:31 +0000

    update and new  calculae, discuss, project, welcome

---
+++ rackx/application/controllers/calculate.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Calculate extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+	}
+
+	public function index() {
+		$data = array();
+		
+// 		$data['']
+// 		echo curl_get('http://tw.screener.finance.yahoo.net/screener/check.html?symid=6169');
+		
+// 		$layout_data['main_area'] = $this->load->view('index', $data, true);
+// 		$this->load->view('layout/main_layout', $layout_data);
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
